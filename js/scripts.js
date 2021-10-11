c2008fd90f9e4c6abc4ae9f29f9f8217b314a3cb
NOLA-sk <81240021+NOLA-sk@users.noreply.github.com>
2021-10-11 19:47:37 +0700

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 800;
 let totalPrice = 0;
 
@@ -11,4 +13,7 @@ schemeSvg.addEventListener('click', (event) => {
         totalPriceTag.textContent = totalPrice;
         console.log(schemeSvg.querySelectorAll(".active").length);
     }
+});
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle("is-open");
 });
